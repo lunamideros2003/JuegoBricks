45f5b993f5771a7cd63c1a29199571f4aaf58bc6
lunamideros2003 <luna.mideros@campusucc.edu.co>
2025-03-26 02:55:55 +0000

    test vocabulario

---
+++ tests/test_vocabulario.py
@@ -0,0 +1,6 @@
+import pytest
+from vocabulario import contar_vocales
+assert contar_vocales("Hola") == 2
+assert contar_vocales("Python") == 1
+assert contar_vocales("aeiou") == 5
+assert contar_vocales("bcdfg") == 0
